fa10b79e42468bbc97a66e0e6829597c9d6d3416
RodResq <rodrigoresque@yahoo.com.br>
2023-04-30 10:48:04 -0300

    Explora Optional lib

--- src/test/OptionslTest.java
+++ src/test/OptionslTest.java
@@ -201,6 +201,23 @@ public class OptionslTest {
     }
 
 //    TODO Continuar daqui: https://www.baeldung.com/java-optional#flat-map
+    @Test
+    public void testFlatMap() {
+        Person person = new Person("jhon", 26);
+        Optional<Person> personOptional = Optional.of(person);
+
+        Optional<Optional<String>> nameOptionalWrapper = personOptional.map(Person::getName);
+        Optional<String> nameOptional = nameOptionalWrapper.orElseThrow(IllegalArgumentException::new);
+        String name1 = nameOptional.orElse("");
+
+        assertEquals("jhon", name1);
+
+        String retorno = personOptional
+                .flatMap(Person::getName)
+                .orElse("");
+
+        assertEquals("jhon", retorno);
+    }
 
     private boolean priceIsInRange1(Modem modem) {
         boolean isInRange = false;
@@ -233,5 +250,42 @@ class Modem {
 
     public Double getPrice() {
         return price;
+    }
+}
+
+
+class Person {
+    private String name;
+    private int age;
+    private String password;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Optional<String> getName() {
+        return Optional.ofNullable(name);
+    }
+
+    public Optional<Integer> getAge() {
+        return Optional.ofNullable(age);
+    }
+
+    public Optional<String> getPassword() {
+        return Optional.ofNullable(password);
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
