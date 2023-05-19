aa611f2e6e1dd467557be3d4ca700ec2f9950f31
RodResq <rodrigoresque@yahoo.com.br>
2023-05-18 22:56:03 -0300

    Explora Converso de Calendar para LocalDateTime

--- src/test/apiData/ConversaoDateAndCalendarToLocalDateTimeTest.java
+++ src/test/apiData/ConversaoDateAndCalendarToLocalDateTimeTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
 
 public class ConversaoDateAndCalendarToLocalDateTimeTest {
@@ -11,6 +12,13 @@ public class ConversaoDateAndCalendarToLocalDateTimeTest {
     @Test
     public void testLocalDateToLocalDateTime() {
         LocalDateTime retorno = LocalDateTime.ofInstant(new Date().toInstant(), ZoneId.systemDefault());
+
+        System.out.println(retorno);
+    }
+
+    @Test
+    public void testCalendarToLocalDateTime() {
+        LocalDateTime retorno = LocalDateTime.ofInstant(Calendar.getInstance().toInstant(), ZoneId.systemDefault());
 
         System.out.println(retorno);
     }
